9c411025928e2a93458231b6f098204f4264fedb
FabioGodoyy <fabiogodoy@Fabio>
2023-09-19 11:00:41 -0300

    fdp

---
+++ app/analytics/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+   
+    <main className="flex min-h-screen bg-white text-black flex-col items-center justify-between p-24">
+      <h1>Analytics page 2</h1>
+      <p>haha</p>
+      <Link href="/">BORA</Link>
+    </main>
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import Image from 'next/image'
+import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+				<title>My website</title>
+        <meta name="google-site-verification" content="hVukCbXKHPDfOGcmihJ7C63X_Q6JzVAMrnG8jI4RAR8" />
+			</Head>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Analytics</h1>
       <p>haha</p>
+      <Link href="/analytics">BORA</Link>
     </main>
+    </>
   )
 }
